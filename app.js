134f9c0c4e37021a235c111a161d2d7b64a008a6
Kyoshim <chaugne.christophe@gmail.com>
2015-06-25 19:03:04 +0200

    update ux probe

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 var stats = require('./lib/stats'),
     docs = require('./lib/docs'),
     tasks = require('./lib/tasks'),
-    versions = require('./lib/versions');
+    versions = require('./lib/versions'),
+    pmx = require('pmx'),
+    pm2 = require('pm2'),
+    shelljs = require('shelljs'),
+    fs      = require('fs'),
+    path    = require('path');
+
+
+var conf = pmx.initModule({
+
+  pid              : pmx.resolvePidPaths(['/var/run/elasticsearch/elasticsearch.pid']),
+
+  widget : {
+    type             : 'generic',
+    logo             : 'https://www.elastic.co/static/img/logo-elastic.png',
+
+    // 0 = main element
+    // 1 = secondary
+    // 2 = main border
+    // 3 = secondary border
+    theme            : ['#33CACC', '#39BDB1', 'white', 'white'],
+
+    el : {
+      probes  : true,
+      actions : true
+    },
+
+    block : {
+      actions : true,
+      issues  : true,
+      main_probes : ['Elastic status', 'Total shards', 'Indexes created', 'Store size']
+    }
+
+    // Status
+    // Green / Yellow / Red
+  }
+});
