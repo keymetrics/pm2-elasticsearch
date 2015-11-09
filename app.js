9659aa0d2bdb5199c05cd668588e6d4d64583745
Unitech <strzelewicz.alexandre@gmail.com>
2015-11-09 11:30:21 +0100

    add error rate probe

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var pmx = require('pmx');
 
-var conf = pmx.initModule({
+pmx.initModule({
 
   pid              : pmx.resolvePidPaths(['/var/run/elasticsearch/elasticsearch.pid',
                                           '/var/run/elasticsearch.pid']),
-
   widget : {
     type             : 'generic',
     logo             : 'https://www.elastic.co/static/img/logo-elastic.png',
@@ -29,37 +28,44 @@ var conf = pmx.initModule({
     // Status
     // Green / Yellow / Red
   }
-});
+}, function(err, conf) {
+  var stats = require('./lib/stats'),
+      docs = require('./lib/docs'),
+      tasks = require('./lib/tasks'),
+      versions = require('./lib/versions'),
+      request = require('request'),
+      shelljs = require('shelljs'),
+      fs      = require('fs'),
+      path    = require('path'),
+      actions = require('./lib/actions'),
+      Probe = pmx.probe();
 
-var stats = require('./lib/stats'),
-    docs = require('./lib/docs'),
-    tasks = require('./lib/tasks'),
-    versions = require('./lib/versions'),
-    request = require('request'),
-    shelljs = require('shelljs'),
-    fs      = require('fs'),
-    path    = require('path'),
-    actions = require('./lib/actions'),
-    Probe = pmx.probe();
+  global.errorRate = Probe.meter({
+    name : 'Error rate'
+  });
 
-request(conf.es_url, function (error, response, body) {
-  if (error)
-    throw new Error('Cannot connect to ' + conf.es_url + ' elastic node');
-});
+  request(conf.es_url, function (error, response, body) {
+    if (error) {
+      errorRate.mark();
+      throw new Error('Cannot connect to ' + conf.es_url + ' elastic node');
+    }
+  });
 
-request(conf.es_url + '/_nodes/_local/settings', function (error, response, body) {
-  var result = JSON.parse(body);
+  request(conf.es_url + '/_nodes/_local/settings', function (error, response, body) {
+    var result = JSON.parse(body);
 
-  var node = Object.keys(result.nodes)[0];
-  esName.set(result.nodes[node].name);
-});
+    var node = Object.keys(result.nodes)[0];
+    esName.set(result.nodes[node].name);
+  });
 
-var esName = Probe.metric({
-  name: 'Node name',
-  value : function() { return 'N/A'; }
-});
+  var esName = Probe.metric({
+    name: 'Node name',
+    value : function() { return 'N/A'; }
+  });
 
-pmx.action('restart', function(reply) {
-  var child = shelljs.exec('/etc/init.d/elasticsearch restart');
-  return reply(child);
+  pmx.action('restart', function(reply) {
+    var child = shelljs.exec('/etc/init.d/elasticsearch restart');
+    return reply(child);
+  });
+
 });
