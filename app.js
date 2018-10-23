3ba97b60ef3b5679b71fe6d2be7e58f949b6ff40
vmarchaud <contact@vmarchaud.fr>
2018-10-23 11:20:03 +0200

    add some metrics

--- app.js
+++ app.js
@@ -4,35 +4,35 @@
  * can be found in the LICENSE file.
  */
 
-var pmx             = require('pmx');
-var elasticsearch   = require('elasticsearch');
-var Probe           = pmx.probe();
-var stats           = require('./lib/stats');
-var Actions         = require('./lib/actions');
+const io = require('@pm2/io')
+const elasticsearch = require('elasticsearch')
+const stats = require('./lib/stats')
+const Actions = require('./lib/actions')
 
-pmx.initModule({
-
-  pid              : pmx.resolvePidPaths(['/var/run/elasticsearch/elasticsearch.pid',
-                                          '/var/run/elasticsearch.pid']),
-  widget : {
-    type             : 'generic',
+io.initModule({
+  widget: {
+    type: 'generic',
     logo: 'https://www.elastic.co/static/img/logo-elastic.png',
-    theme            : ['#39bdb1', '#1B2228', 'white', '#807C7C'],
-    el : {
-      probes  : true,
-      actions : true
+    theme: ['#39bdb1', '#1B2228', 'white', '#807C7C'],
+    el: {
+      probes: true,
+      actions: true
     },
 
-    block : {
-      issues  : true,
-      meta : true,
-      main_probes : ['Elastic status', 'Nodes', 'Shards', 'Indices', 'Documents', 'Store size']
+    block: {
+      issues: true,
+      meta: true,
+      main_probes: ['Elastic status', 'Nodes', 'Shards', 'Indices', 'Documents', 'Store size']
     }
   }
-}, function(err, conf) {
-  var ELASTICSEARCH_URI     = conf.elasticsearchUri || process.env.PM2_ELASTICSEARCH_URI;
+}, function (err, conf) {
+  if (err) {
+    console.error(err)
+    return process.exit(1)
+  }
+  const ELASTICSEARCH_URI = conf.elasticsearchUri || process.env.PM2_ELASTICSEARCH_URI
 
-  var client = new elasticsearch.Client({
+  const client = new elasticsearch.Client({
     host: ELASTICSEARCH_URI,
     log: 'error'
   })
@@ -46,7 +46,7 @@ pmx.initModule({
     return stats.update(client)
   }, 1000)
 
-  /** Register PMX actions */
-  var actions = new Actions(client)
+  // Register PMX actions
+  const actions = new Actions(client)
   actions.register()
 })
